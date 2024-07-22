59e7b21c438c926959421ebd2ee619e1404c20de
mrrtedodee <geographychristianflags2@gmail.com>
2024-07-22 13:12:41 +1000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -25,7 +25,6 @@ addLayer("p", {
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true}
-}),
 upgrades: {11: {
 
-},},
+},},}),
